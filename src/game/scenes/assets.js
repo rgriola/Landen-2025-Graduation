b0d54b65578acf489b5641bd91430d4a51355a8e
rgriola <94720986+rgriola@users.noreply.github.com>
2025-06-15 22:56:39 -0400

    Try Again

---
+++ src/game/scenes/assets.js
@@ -0,0 +1,905 @@
+// AUTO-GENERATED FILE. Edit only with generateAssetsJson.js!
+export const images = [
+    {
+        "key": "1974",
+        "path": "1974.jpg",
+        "label": "NEED LABEL",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "1974_East",
+        "path": "1974_East.png",
+        "label": "1974 East",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "Dad_Me_San_Diego",
+        "path": "Dad_Me_San_Diego.png",
+        "label": "The Zoo",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "Gilla_",
+        "path": "Gilla_.png",
+        "label": "Gilla",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "HS_Photo-2",
+        "path": "HS_Photo-2.png",
+        "label": "Smiley",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG00368",
+        "path": "IMG00368.png",
+        "label": "Snow Bird",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_0012",
+        "path": "IMG_0012.png",
+        "label": "Actors Guild",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_0055",
+        "path": "IMG_0055.png",
+        "label": "The Maple Albumn",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_0066",
+        "path": "IMG_0066.png",
+        "label": "Back Packs to The Future",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_0075",
+        "path": "IMG_0075.png",
+        "label": "Hey Cow",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_0086",
+        "path": "IMG_0086.png",
+        "label": "Cupcake",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_0094",
+        "path": "IMG_0094.png",
+        "label": "Pizza De Pazza",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_0109",
+        "path": "IMG_0109.png",
+        "label": "First Day",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_0136",
+        "path": "IMG_0136.png",
+        "label": "Where is Puppy?",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_0208",
+        "path": "IMG_0208.png",
+        "label": "Purple",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_0231",
+        "path": "IMG_0231.png",
+        "label": "High Steppers",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_0528",
+        "path": "IMG_0528.png",
+        "label": "Performers",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_0544",
+        "path": "IMG_0544.png",
+        "label": "Laughers",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_0568",
+        "path": "IMG_0568.png",
+        "label": "Harry Potter",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_0838",
+        "path": "IMG_0838.png",
+        "label": "Bad Elf",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_0994",
+        "path": "IMG_0994.png",
+        "label": "Joey",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_1066",
+        "path": "IMG_1066.png",
+        "label": "Yea No",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_1257",
+        "path": "IMG_1257.png",
+        "label": "The Band",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_1377",
+        "path": "IMG_1377.png",
+        "label": "San Fran",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_1448",
+        "path": "IMG_1448.png",
+        "label": "More Piggies",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_1590",
+        "path": "IMG_1590.png",
+        "label": "You are Number One",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_1602",
+        "path": "IMG_1602.png",
+        "label": "Feet",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_1610",
+        "path": "IMG_1610.png",
+        "label": "Cupcakes",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_1626",
+        "path": "IMG_1626.png",
+        "label": "Ah Family",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_1640",
+        "path": "IMG_1640.png",
+        "label": "Second Trip",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_1664",
+        "path": "IMG_1664.png",
+        "label": "Baby",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_1667",
+        "path": "IMG_1667.png",
+        "label": "Cupcak-Check",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_1672",
+        "path": "IMG_1672.png",
+        "label": "Baby More",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_1742",
+        "path": "IMG_1742.png",
+        "label": "Goal!!",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_1816",
+        "path": "IMG_1816.png",
+        "label": "Jake",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_1844",
+        "path": "IMG_1844.png",
+        "label": "California",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_2119",
+        "path": "IMG_2119.png",
+        "label": "Missing Gerbal",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_2122",
+        "path": "IMG_2122.png",
+        "label": "Big Shake",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_2176",
+        "path": "IMG_2176.png",
+        "label": "Arizona",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_2178",
+        "path": "IMG_2178.png",
+        "label": "More Arizona",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_2371",
+        "path": "IMG_2371.png",
+        "label": "Glad Hands",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_2379",
+        "path": "IMG_2379.png",
+        "label": "Our Doggie",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_2506",
+        "path": "IMG_2506.png",
+        "label": "Huggies",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_2767",
+        "path": "IMG_2767.png",
+        "label": "Grandpa Bob",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_3047",
+        "path": "IMG_3047.png",
+        "label": "Christmas Time",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_3051",
+        "path": "IMG_3051.png",
+        "label": "Jon Oliver Fanclub",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_3790",
+        "path": "IMG_3790.png",
+        "label": "Rainbows Over",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_3792",
+        "path": "IMG_3792.png",
+        "label": "MTV",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_3948",
+        "path": "IMG_3948.png",
+        "label": "Nose",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_4019",
+        "path": "IMG_4019.png",
+        "label": "NEED LABEL",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_4021",
+        "path": "IMG_4021.png",
+        "label": "NEED LABEL",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_4073",
+        "path": "IMG_4073.png",
+        "label": "Brown Belt",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_4075",
+        "path": "IMG_4075.png",
+        "label": "Safety First",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_4153",
+        "path": "IMG_4153.png",
+        "label": "Cutie",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_4193",
+        "path": "IMG_4193.png",
+        "label": "4 Cheese Souffle",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_4422",
+        "path": "IMG_4422.png",
+        "label": "DC Feelings",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_4423",
+        "path": "IMG_4423.png",
+        "label": "Yoda",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_4453",
+        "path": "IMG_4453.png",
+        "label": "Hi",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_4500",
+        "path": "IMG_4500.png",
+        "label": "ADK",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_5005",
+        "path": "IMG_5005.png",
+        "label": "Grandparent",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_5311",
+        "path": "IMG_5311.png",
+        "label": "Another Diner",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_5402",
+        "path": "IMG_5402.png",
+        "label": "Blue Suede Shoes",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_5469",
+        "path": "IMG_5469.png",
+        "label": "Baby Blue Eyes",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_5495",
+        "path": "IMG_5495.png",
+        "label": "Yoguurrrt",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_6238",
+        "path": "IMG_6238.png",
+        "label": "Family Time",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_6254-2",
+        "path": "IMG_6254-2.png",
+        "label": "Children of the Corn",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_6335",
+        "path": "IMG_6335.png",
+        "label": "Brooke & Landen",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_6586",
+        "path": "IMG_6586.png",
+        "label": "Central Perk",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_6686",
+        "path": "IMG_6686.png",
+        "label": "The Old Passy",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_6780",
+        "path": "IMG_6780.png",
+        "label": "Cafe Du Monde",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_7211",
+        "path": "IMG_7211.png",
+        "label": "Litte Reporter",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_7274",
+        "path": "IMG_7274.png",
+        "label": "Punch Up",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_7287",
+        "path": "IMG_7287.png",
+        "label": "Battery Brain",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_7292",
+        "path": "IMG_7292.png",
+        "label": "NYC Fishing",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_7295",
+        "path": "IMG_7295.png",
+        "label": "Joey Da Piggy",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_7296",
+        "path": "IMG_7296.png",
+        "label": "Joey",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_7405",
+        "path": "IMG_7405.png",
+        "label": "Little Minion",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_7479",
+        "path": "IMG_7479.png",
+        "label": "Random Cat",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_7571",
+        "path": "IMG_7571.png",
+        "label": "Hi There",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_7573",
+        "path": "IMG_7573.png",
+        "label": "A Line in the Ground",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_7916",
+        "path": "IMG_7916.png",
+        "label": "Working Hard",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_8229",
+        "path": "IMG_8229.png",
+        "label": "Gopher",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_8297 2",
+        "path": "IMG_8297 2.png",
+        "label": "Where is My Cello?",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_8368",
+        "path": "IMG_8368.png",
+        "label": "Angus The Runner",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_8977",
+        "path": "IMG_8977.png",
+        "label": "Violent Femmes",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_9039",
+        "path": "IMG_9039.png",
+        "label": "Bedtime",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_9259",
+        "path": "IMG_9259.png",
+        "label": "Deadpool 3",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_9325",
+        "path": "IMG_9325.png",
+        "label": "Awwwwww",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_9357",
+        "path": "IMG_9357.png",
+        "label": "Morning",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_9407",
+        "path": "IMG_9407.png",
+        "label": "Coffee Time",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_9710",
+        "path": "IMG_9710.png",
+        "label": "Yellow Shirt",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_9783",
+        "path": "IMG_9783.png",
+        "label": "Kodak Moment",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_9968",
+        "path": "IMG_9968.png",
+        "label": "Banana Time",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "IMG_9979",
+        "path": "IMG_9979.png",
+        "label": "Pats Steaky",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "Landen_09212007-6_2",
+        "path": "Landen_09212007-6_2.png",
+        "label": "New Blue Eyes",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "Landen_Clothes",
+        "path": "Landen_Clothes.png",
+        "label": "Thrifty!",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "Lea_Dog_Stroller",
+        "path": "Lea_Dog_Stroller.png",
+        "label": "Lea & Jake",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "Love_Me",
+        "path": "Love_Me.png",
+        "label": "Love Me",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "Love_Sign",
+        "path": "Love_Sign.png",
+        "label": "Love",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "Lucky_Man",
+        "path": "Lucky_Man.png",
+        "label": "I am I am",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "Moon",
+        "path": "Moon.png",
+        "label": "NEED LABEL",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "Ron_Burgundy",
+        "path": "Ron_Burgundy.png",
+        "label": "Reporter",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "SaraZoe Aunt Ida's_07-14-2007_66",
+        "path": "SaraZoe Aunt Ida's_07-14-2007_66.png",
+        "label": "Aunt Ida",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "Zoe_Baptsism-57",
+        "path": "Zoe_Baptsism-57.png",
+        "label": "2007",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "bg",
+        "path": "bg.png",
+        "label": "background",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "cameraman",
+        "path": "cameraman.jpg",
+        "label": "Camera Woman",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "dadandme",
+        "path": "dadandme.png",
+        "label": "Dad and Me",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "dakotalove",
+        "path": "dakotalove.jpg",
+        "label": "Dakota Love",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "handholders",
+        "path": "handholders.png",
+        "label": "Handhold Love",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "jailbird",
+        "path": "jailbird.jpg",
+        "label": "Jailbird",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "landen_spoon",
+        "path": "landen_spoon.png",
+        "label": "Spoons",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "logo",
+        "path": "logo.png",
+        "label": "Logo",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "lucky",
+        "path": "lucky.png",
+        "label": "Luck Man",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "packaderm",
+        "path": "packaderm.png",
+        "label": "Umngani",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "samenrosendave",
+        "path": "samenrosendave.jpg",
+        "label": "Rose, Sam & Dave",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "sez-me-ice-cream",
+        "path": "sez-me-ice-cream.png",
+        "label": "Twist & Sprinkles",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "shitter",
+        "path": "shitter.png",
+        "label": "Elf On The Throne",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "sisters",
+        "path": "sisters.jpg",
+        "label": "Sibby's",
+        "scale": 1,
+        "speed": 1
+    },
+    {
+        "key": "thelegend",
+        "path": "thelegend.jpg",
+        "label": "The Legend",
+        "scale": 1,
+        "speed": 1
+    }
+];
+export const particles = [
+    {
+        "key": "red_particle",
+        "path": "red_particle.png",
+        "label": "NEED LABEL",
+        "scale": 1
+    }
+];
+export const audio = [
+    {
+        "key": "backgroundMusic",
+        "path": "Nu_Alkemi_t_East_London_instrumental_3_05.mp3"
+    }
+];
+export const videos = [
+    {
+        "key": "background_Puffins",
+        "path": "PuffinsDownTown.mp4",
+        "scale": 1,
+        "loop": false,
+        "muted": false,
+        "autoplay": false
+    },
+    {
+        "key": "background_rivertimelapse",
+        "path": "rivertimelapse.mp4",
+        "scale": 1,
+        "loop": false,
+        "muted": false,
+        "autoplay": false
+    }
+];
+
+// === MANUAL ASSETS CODE BELOW ===
+export const ASSETS = {
+    images,
+    particles,
+    audio,
+    videos,
+    preloadAll: function(scene) {
+        // Set path
+        scene.load.setPath('assets');
+        console.log('Assets PreloadALL loaded!');
+
+        // Load all images
+        this.images.forEach(img => {
+            scene.load.image(img.key, img.path || `${img.key}.png`);
+        });
+            // Load all particle images
+        if (this.particle) {
+            this.particle.forEach(particle => {
+                scene.load.image(particle.key, particle.path || `${particle.key}.png`);
+            });
+        }
+        // Load all audio
+        this.audio.forEach(audio => {
+            scene.load.audio(audio.key, audio.path);
+        });
+        
+        // Load all videos with proper settings
+        this.videos.forEach(video => {
+            scene.load.video(video.key, video.path, video.loop || false, video.muted || false);
+        });
+    }
+};
+
+
+
+
